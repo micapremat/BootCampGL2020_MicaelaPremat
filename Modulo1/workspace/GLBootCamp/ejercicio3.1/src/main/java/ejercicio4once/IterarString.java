77f70178102b86a3da9c8b0206e89688902dc11d
Micaela Premat <micapremat@gmail.com>
2020-07-23 15:01:32 -0300

    ejercicio 4.11

---
+++ Modulo1/workspace/GLBootCamp/ejercicio3.1/src/main/java/ejercicio4once/IterarString.java
@@ -0,0 +1,13 @@
+package ejercicio4once;
+
+public class IterarString {
+
+	public static void main(String[] args) {
+		String cadena = "Bootcamp 2020";
+		for(int i=0; i<cadena.length(); i++) {
+			System.out.println("Letra: "+cadena.charAt(i));
+		}
+
+	}
+
+}
