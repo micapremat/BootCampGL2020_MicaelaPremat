b250e201a5ae3016a315924fef2c44a3649e37af
Micaela Premat <micapremat@gmail.com>
2020-07-28 14:42:58 -0300

    proyecto inicial

---
+++ TrabajoIntegrador/trabajoIntegrador/src/main/java/ar/com/gl/shop/product/repository/App.java
@@ -0,0 +1,13 @@
+package ar.com.gl.shop.product.repository;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
